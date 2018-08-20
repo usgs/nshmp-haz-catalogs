14b131011f28abaa730512e3647ba6d43481ea42
bclayton-usgs <bclayton@usgs.gov>
2018-08-20 08:22:04 -0600

    Wrtie to CSV and ascii file

---
+++ src/agrid/adaptive/agrid_functions.h
@@ -0,0 +1,33 @@
+// all function declarations here
+
+// c functions
+void print_param_format(void);
+//int check_args(char *parf, char *parf_format, int *newp);
+
+/* ck_args_wt_results */
+int check_args(int argc, char **argv, char *paramf, char *summf, char *paramf_format, int *stop_run, int *newp);
+void write_results_to_file(struct smoothp* smp, struct rates* seisr, struct catalog * cat, struct boundaries *boundR, char *outf);
+
+int read_params_new_old(char *parf, struct compl_levels* cat_comp, struct boundaries* reg, struct catalog* cat, struct smoothp* smp, char *outfnm, int new_style_formatting);
+int read_params(char *parf, struct compl_levels* cat_comp, struct boundaries* reg, struct catalog* cat, struct smoothp* smp, char *outfnm);
+int read_params_new(char *parf, struct compl_levels* cat_comp, struct boundaries* reg, struct catalog* cat, struct smoothp* smp, char *outfnm);
+void Parse_Text(FILE *fp, const char *querystring, const char type, void* result);
+void write_params(struct compl_levels* cat_comp, struct boundaries* reg, struct catalog* cat, struct smoothp* smp1, char *outfnm) ;
+void write_params_to_file(char *fnm, struct compl_levels* cat_comp, struct boundaries* reg, struct catalog* cat, struct smoothp* smp1, char *outfnm);
+void init_dist_rates(struct eqcat* catn, float atot[], struct rates* seisr);
+int get_eqs_fm_catalog(char *fnm, struct eqcat* catn, struct compl_levels* compl, struct boundaries* reg, struct rates* seisr);
+void get_eq_neighbor_dists(struct eqcat* catn);
+void write_eq_neighbor_dists(struct eqcat* catn, struct smoothp* smp);
+void smooth_seis_rates(struct rates* seisr, struct smoothp* smp, struct eqcat* catn, struct boundaries* reg, struct compl_levels* cat_comp, struct catalog* cat);
+float calc_seis_rate(struct smoothp* smpp, struct eqcat* eqcatp, struct compl_levels* cat_compp, struct catalog* cat_parp, int eqno);
+void convert_eq_nos_rates(struct rates* seisr, struct compl_levels* compl, struct smoothp* smp, struct catalog* cat, struct boundaries* reg);
+float get_sigma_value(struct smoothp* smp, struct eqcat* catn, int eqno);
+
+/* write_asum_to_file */
+void write_asum_to_file(struct rates* seisr, char *fbasenm, struct catalog * catv, float mag);
+void write_asum_to_csvfile(struct rates* seisr, char *fbasenm, struct catalog * catv, struct boundaries *boundR, float mag);
+void write_unsmoothed_asum_to_file(struct rates* seisr, char *fbasenm);
+
+void initialize_params(struct compl_levels* cat_comp, struct boundaries* reg, struct catalog* cat, struct smoothp* smp);
+void read_completenessVals(struct compl_levels* cat_comp);
+void assign_variable_completeness_eqs(struct eqcat* cat, struct compl_levels* compl_vals);
---
+++ src/agrid/adaptive/ck_args_wt_results.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "agrid.h"
+#include "agrid_functions.h"
+
+
+/*--------------------------------------------------------------------------*/
+int check_args(int argc, char **argv, char *paramf, char *summf, char *paramf_format, int *stop_run, int *newp)
+/*--------------------------------------------------------------------------*/
+{
+/* CHECK INPUT ARGUMENTS */
+  if (  (argc != 2) && (argc != 4) && (argc !=5)  ) {
+    fprintf(stderr,"USAGE: %s [parameter file] [parameter file formatting (new/old)] [summary file name] [[ optional quit before run (1) ]]\n", argv[0]);
+    fprintf(stderr,"For parameter file formatting, enter -h, as single command line argument.\n");
+    fprintf(stderr,"Enter (new/old) to specify the formatting style for the parameter file.\n");
+    fprintf(stderr,"Optional quit argument will stop run before seismicity rate calculations to permit check of parameters.\n"); exit(1);
+  }
+  sscanf(argv[1],"%s",paramf);
+  if ( argc > 2) {
+    sscanf(argv[2],"%s",paramf_format);
+    sscanf(argv[3],"%s",summf);
+    if (argc > 4) sscanf(argv[4],"%d", stop_run);
+  }
+
+// check for help option
+  if ( STREQ(paramf,"-h") ) {
+    print_param_format();
+    return 1;
+  }
+// check pareter file formatting
+  if ( STREQ(paramf_format,"new") ) {
+    fprintf(stderr,"Using new parameter file formatting...\n");
+    *newp=1;
+    return 0;
+  }
+  else if ( STREQ(paramf_format,"old") ) {
+    fprintf(stderr,"Using old parameter file formatting...\n");
+    *newp=0;
+    return 0;
+  }
+  else {
+    fprintf(stderr,"User must enter 'new' or 'old' to specify the style of formatting for the parameter file - %s.\n", paramf_format);
+    return 1;
+  }
+}
+
+
+/*--------------------------------------------------------------------------*/
+void write_results_to_file(struct smoothp* smp, struct rates* seisr, struct catalog * cat, struct boundaries *boundR, char *outf)
+/*--------------------------------------------------------------------------*/
+{
+  int cnt;
+  float magv;
+  float tmpsum=0.0;
+
+// quick check of earthquake numbers
+  for (cnt=0; cnt<seisr->nv; cnt++) {
+    tmpsum=tmpsum+seisr->asum[cnt]*7;
+  }
+  fprintf(stderr,"tmpsum (no. M0+ eqs in 7 years) = %.4f\n", tmpsum);
+
+// if a smoothed seismicity model is generated, output results at M0(10^a), M4, M5 and unsmoothed model
+  if ( smp->apply_sm ) {
+// M0, 10^a values
+    magv=0.0;
+    write_asum_to_file(seisr, outf, cat, magv);
+    fprintf(stderr,"Writing to csv file...\n");
+    write_asum_to_csvfile(seisr, outf, cat, boundR, magv);
+// M0, 10^a, un-smoothed 
+//    write_unsmoothed_asum_to_file(seisr, outf);
+
+// M3.0, 10^(a-3.0b) values
+/*
+*    magv=3.0;
+*    write_asum_to_file(seisr, outf, cat, magv);
+// M3.5, 10^(a-3.5b) values
+*    magv=3.5;
+*    write_asum_to_file(seisr, outf, cat, magv);
+// M4, 10^(a-4b) values
+*    magv=4.0;
+*    write_asum_to_file(seisr, outf, cat, magv);
+// M4.5, 10^(a-4.5b) values
+*    magv=4.5;
+*    write_asum_to_file(seisr, outf, cat, magv);
+*
+// M5, 10^(a-5b) values
+*    magv=5.0;
+*    write_asum_to_file(seisr, outf, cat, magv);
+*/    
+  }
+  else {
+    magv=0.0;
+    write_asum_to_file(seisr, outf, cat, magv);
+    magv=cat->minMag;
+    write_asum_to_file(seisr, outf, cat, magv);
+  }
+}
+
+
+
